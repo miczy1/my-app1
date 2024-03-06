a53db035f861e0c51f6a1aa2202e216c641d0034
dawid.senko <dsenko26@gmail.com>
2024-03-06 13:47:42 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [carName, setCarName] = useState('');
+    const [cars, setCars] = useState([]);
+
+    const handleChange = (event) => {
+        setCarName(event.target.value);
+    };
+
+    const deleteCar = (index) => {
+        const updatedCars = [...cars]
+        updatedCars.splice(index, 1)
+        setCars(updatedCars)
+    }
+    const addCar = () => {
+    setCars([...cars, carName]);
+    setCarName('');
+    }
+
+    return(
+        <div>
+            <p>Enter car name: </p>
+            <input className='car' value={carName} onChange={handleChange}/>
+            <button onClick={addCar}>ADD</button>
+            <ul>
+            {cars.map((car, index) => (
+                <li key={index}>
+                    {car}
+                    <button onClick={() => deleteCar(index)}>DELETE</button>
+                </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
